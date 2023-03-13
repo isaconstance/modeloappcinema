bc08413c4dce748351f9981ef95d2f1e1670a42e
Isabella <isonapro@gmail.com>
2023-03-13 20:27:38 -0300

    rotas do cadastrar

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/cadastro-filme', function(){
+    return view('cadastroFilme');
+});
+
