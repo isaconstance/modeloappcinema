aec0eb3675e6329cb3d1fbeef3ea88bea1065376
Natasha <isonapro@gmail.com>
2023-03-21 22:48:17 -0300

    iniciando banco de dados

---
+++ app/Models/cadastroFuncionarioModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class cadastroFuncionarioModel extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'nome_fun', 'email_fun', 'whatsapp_fun', 'cpf_fun', 'senha_fun'
+    ];
+}
